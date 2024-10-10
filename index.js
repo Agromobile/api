7a298a5eb8366d9dd1649db0d7dbd49a76993830
Bankole Azeez Babatunde <bankoleazeezb98@gmail.com>
2024-10-11 00:35:27 +0100

    protected added

--- index.js
+++ index.js
@@ -176,6 +176,22 @@ app.post('/login/personal', (req, res) => {
 });
 
 //Middleware to protect routes
+const authenticateToken = (req, res, next) => {
+
+  const token = req.cookies.token;
+  if (!token) 
+    {
+      return res.status(401).json({message: 'unauthorized'});
+    }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({message: 'invalid token'});
+    }
+    req.user = user;
+    next();
+  });
+}
 
 //port connection
 app.listen(port, () => {
