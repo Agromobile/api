3952583190cc240ba1c90263ad30d55b7c36df34
Azeezbank <159575592+Azeezbank@users.noreply.github.com>
2024-10-12 19:16:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -230,10 +230,14 @@ app.get('/get/product', authenticateToken, (req, res) => {
 
   db.query(sql, [userid], (err, result) => {
     if (err) {
-      res.status(404).json({message: 'Not found'})
+      console.error('Error fetching product', err);
+      return res.status(500).json({message: 'Error fetching product'});
+    }
+    if (result.length === 0) {
+      return res.status(404).json({message: 'No product found'})
     }
     res.status(200).json(result)
-  })
+  });
 });
 
 
