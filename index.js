79f542093c3937fb03864df5927c2bfcde5d4d49
Azeezbank <159575592+Azeezbank@users.noreply.github.com>
2024-10-16 21:47:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -192,6 +192,7 @@ app.post('/login/personal', (req, res) => {
 const authenticateToken = (req, res, next) => {
 
   const token = req.cookies.token;
+  console.log(token);
   if (!token) 
     {
       return res.status(401).json({message: 'unauthorized'});
